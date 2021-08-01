4ae2d8ec8c408b3c09911fdf139e7ac52ef6a289
chinmayajha <84258488+chinmayajha@users.noreply.github.com>
2021-08-01 16:17:27 +0530

    zz

---
+++ A_Alloy.cpp
@@ -0,0 +1,27 @@
+
+#include "bits/stdc++.h"
+#pragma GCC optimize "trapv"
+using i64 = long long int;
+using namespace std;
+int nxt(){int x; cin >> x; return x;}
+#define sz(x) ((int)(x).size()) 
+#define all(x) (x).begin(), (x).end()
+#define inarr(a,n) for(int i = 0; i < n; ++i) cin >> a[i];
+//
+void solve(){
+    int n;cin >> n;
+    // generate_n(a.begin(), n, nxt);
+    
+}
+
+int main(){
+    cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);
+    int testt = 1;
+    // cin >> testt;
+    for (int i = 0; i < testt; ++i) {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
+
+
